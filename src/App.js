4868dd64e4d1e4038874aa838e3ad84b69287d99
Ashrof Salman <salmanashraf853@gmil.com>
2021-12-28 15:59:55 +0100

    adding main files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles/App.scss';
+import { useEffect } from 'react';
+
+function App() {
+  
+
+  useEffect(()=>{
+    
+  })
+
+  return (
+    <div className="App">
+    jj
+    </div>
+  );
+}
+
+export default App;
